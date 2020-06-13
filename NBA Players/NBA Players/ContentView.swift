89a351e7de87513f43008dab0668b75b9824452d
farazhaider88 <farazhaider88@yahoo.com>
2020-06-13 21:01:34 +0500

    assets added

---
+++ NBA Players/NBA Players/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NBA Players
+//
+//  Created by Faraz Haider on 13/06/2020.
+//  Copyright © 2020 Etisalat. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Swift UI!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
